a63f489696b2098e98944637f2abbc1b6274bdca
Kyuwoong Lee <kwooler@gmail.com>
2021-03-10 04:22:31 +0000

    Kyu

--- Assignment2-4.cpp
+++ Assignment2-4.cpp
@@ -9,14 +9,20 @@ test = 'A';
 int counter = 0;
 
 while(test <='Z') {
-  counter++;
-  cout << test << " ";
-  test = test +1;
-  if (counter == 5 ) {
-  counter = 0;
-  cout << '\n';
-  
-  }
+	// counter++;
+	cout << test << " ";
+	test = test +1;
+	// if (counter == 5 ) {
+	//   // for readability, need to keep the indentation rule
+	// 	counter = 0;
+	// 	cout << '\n';
+	// }
+	// Insted of counter,
+	// we can use the "test" var to control "newline"
+	if (test % 5 == 0 ) {
+		cout << '\n';
+	}
+
 }
 
 }
