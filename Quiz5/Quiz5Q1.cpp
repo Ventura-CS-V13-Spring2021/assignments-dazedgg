03d3f76f311ae47ea38607501b1661796414ef93
Zakhar Gazizov <>
2021-04-27 04:41:25 +0000

    finished question 1

---
+++ Quiz5/Quiz5Q1.cpp
@@ -0,0 +1,20 @@
+#include    <iostream>
+using namespace std;
+
+int main()
+{
+    const int SIZE=10;
+    int     numbers[10] = {5, 10, 75, 80, 25, 55, 95, 70, 60, 15};
+    int     diff[10];
+    int     sum=0;
+
+    for (int i = 0; i < SIZE;i++) {
+      sum += numbers[i];
+      cout << numbers[i] << " ";
+    }
+  for (int i=0; i<SIZE; i++) {
+    diff[i] = sum - numbers[i];
+    cout << diff[i] << " ";
+  }
+}
+
