b3f50fef961476d317f3038c80b5bff8714a77ee
Zakhar Gazizov <>
2021-05-03 23:44:20 +0000

    filled integerarray.cpp with the contents on canvas still working on it

---
+++ Assignment 6-1/IntegerArray.cpp
@@ -0,0 +1,45 @@
+#include "IntegerArray.hpp"
+#include <iostream>
+
+
+int IntegerArray::getLength(void) const
+{
+
+    // return length;
+}
+int IntegerArray::getLastelm(void) const
+{
+
+    // return the value of the last element;
+}
+void IntegerArray::printAll(void) const
+{
+
+    // Print all elements
+}
+void IntegerArray::fillUp(void)
+{
+
+    // Make up the array with the random integer values(0 to 100)
+
+    // The array numbers has the maximum capacity with N.
+
+    // Set N to the length.
+}
+void IntegerArray::sortAsc()
+{
+
+    // Sort the array values with ascending order
+}
+void IntegerArray::removeLastelm(void)
+{
+
+    // Decrease 1 from the length
+}
+void IntegerArray::appendElement(int v)
+{
+
+    // Append the value v to the array
+
+    // length += 1;
+}
