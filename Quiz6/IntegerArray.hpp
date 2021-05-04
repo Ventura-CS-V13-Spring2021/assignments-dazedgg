8d818b19caa534bf50926580eb75b3b755872c30
Zakhar Gazizov <>
2021-05-04 01:15:39 +0000

    finished integerarray.hpp

--- Quiz6/IntegerArray.hpp
+++ Quiz6/IntegerArray.hpp
@@ -9,5 +9,12 @@ class IntegerArray {
   int numbers[N];
   int length = 0;
 
+public:
+        int getLength(void) const;
+        void printAll(void) const;
+        void fillUp(void);
+        void sortArray(int flag);
+        void getPrimenumbers(void) const;
 
-}
+};
+#endif
