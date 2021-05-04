d69836e94a9c60b762db72d4c47bb64c4de00df3
Zakhar Gazizov <>
2021-05-04 01:20:27 +0000

    im working on integerarray.cpp but going to obviously edit it i copied it from the other assignment

---
+++ Quiz6/IntegerArray.cpp
@@ -0,0 +1,59 @@
+#include "IntegerArray.hpp"
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+using namespace std;
+
+
+int IntegerArray::getLength(void) const
+{
+
+    return length;
+}
+int IntegerArray::getLastelm(void) const
+{
+
+    return numbers[length-1];
+}
+void IntegerArray::printAll(void) const
+{
+
+    for (int i = 0; i < length; i++) {
+      cout << numbers[i] << "\t";
+    }
+}
+void IntegerArray::fillUp(void)
+{
+  int i;
+  srand(time(0));
+
+  for (int i = 0; i < N; i++) {
+    numbers[i] = rand() % 100 + 1;
+    length = N;
+  }
+   
+}
+void IntegerArray::sortAsc()
+{
+  sort(numbers, numbers + length);
+
+
+
+   
+}
+void IntegerArray::removeLastelm(void)
+{
+
+   length--;
+}
+void IntegerArray::appendElement(int v)
+{
+
+    numbers[length] = v;
+    
+ 
+     length+=1;
+}
+
