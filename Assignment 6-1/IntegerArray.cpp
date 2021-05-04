efbf9b852a1391d2737182d84e3272c69c049419
Zakhar Gazizov <>
2021-05-04 00:09:11 +0000

    I think I finished it not sure.

--- Assignment 6-1/IntegerArray.cpp
+++ Assignment 6-1/IntegerArray.cpp
@@ -7,17 +7,17 @@
 using namespace std;
 
 
-int IntegerArray::getLength(void) 
+int IntegerArray::getLength(void) const
 {
 
     return length;
 }
-int IntegerArray::getLastelm(void)
+int IntegerArray::getLastelm(void) const
 {
 
     return numbers[length-1];
 }
-void IntegerArray::printAll(void) 
+void IntegerArray::printAll(void) const
 {
 
     for (int i = 0; i < length; i++) {
@@ -26,10 +26,10 @@ void IntegerArray::printAll(void)
 }
 void IntegerArray::fillUp(void)
 {
-  
+  int i;
   srand(time(0));
 
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < N; i++) {
     numbers[i] = rand() % 100 + 1;
     length = N;
   }
@@ -54,5 +54,8 @@ void IntegerArray::appendElement(int v)
     numbers[length] = v;
     
  
-     length++;
+     length+=1;
 }
+
+
+// I tried this, I'm not sure if this is right though... I feel like I am missing something.
