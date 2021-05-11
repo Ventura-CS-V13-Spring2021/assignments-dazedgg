f704db160145204a277a0ebdd6300ce70f0db735
Zakhar Gazizov <>
2021-05-11 02:41:58 +0000

    finished dob.cpp

--- DOB.cpp
+++ DOB.cpp
@@ -2,3 +2,53 @@
 #include <DOB.hpp>
 
 using namespace std;
+
+
+DOB::DOB() {
+month = 0;
+day = 0;
+year = 0;
+
+}
+
+DOB::DOB(int m, int d , int y) {
+  month = m;
+  day = d;
+  year = y;
+}
+
+int DOB::getDay()const {
+  return day;
+}
+
+int DOB::getMonth()const{
+  return month;
+}
+
+int DOB::getYear()const {
+  return year;
+}
+
+void DOB::printDate()const{
+
+  cout << day << "/" << month<< "/" << year;
+}
+
+void DOB::setDOB(int m, int d, int y) {
+
+  month = m;
+  day = d;
+  year = y;
+}
+
+void DOB::setMonth(int m) {
+  month = m;
+}
+
+void DOB::setDay(int d) {
+  day = d;
+}
+
+void DOB::setYear(int y) {
+  year = y;
+}
