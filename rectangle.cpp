23360b1f60e973bf47f85ad4a08b6687cc5f4fca
Zakhar Gazizov <>
2021-05-11 03:35:48 +0000

    still working on rectangle.cpp trying to figure out the getArea and center part.

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include "coordinate.hpp"
+#include "rectangle.hpp"
 
 using namespace std;
+
+Coordinate Rectangle::getLB() const {
+  return lb;
+}
+
+Coordinate Rectangle::getRT() const {
+
+  return rt;
+}
+
+
 
