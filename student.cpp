6c8c5752b89c38b4fbc0911050bf31c923fc4afd
Zakhar Gazizov <>
2021-05-11 03:07:29 +0000

    finished assignment 7 with student.cpp

--- student.cpp
+++ student.cpp
@@ -25,3 +25,54 @@ int Student::getID()const {
 
 }
 
+string Student::getSname()const {
+
+  return sname;
+
+}
+
+DOB Student::getDOB()const {
+
+return dob;
+
+}
+
+Address Student::getAddress()const{
+
+return address;
+
+}
+
+void Student::setSname(string name) {
+
+sname = name;
+
+}
+
+void Student::setAddress(Address addr) {
+
+address = addr;
+
+}
+
+void Student::setDOB(DOB dob) {
+
+dob = dob;
+
+}
+
+
+void Student::setID(int num) {
+
+id = num;
+
+
+}
+
+void Student::printStudent(){
+cout << "Student's name and ID Number: " << sname << " " << id << "\t" <<endl;
+cout << "Address and Date of Birth: ";
+address.printAddress();
+dob.printDate();
+cout << "\n\n";
+}
