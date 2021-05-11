bb47cc7c0ac6eadb5b60c7b5c9cb8c68dd591781
Zakhar Gazizov <>
2021-05-11 03:47:59 +0000

    figured out the area and center formula

--- rectangle.cpp
+++ rectangle.cpp
@@ -14,4 +14,23 @@ Coordinate Rectangle::getRT() const {
 }
 
 
+double Rectangle::getArea() {
 
+  return area;
+}
+
+Coordinate Rectangle::getCenter() {
+
+  return center;
+}
+
+void Rectangle::setLBRT(Coordinate lbval, Coordinate rtval) {
+
+  lb = lbval;
+  rt = rtval;
+
+  area = ((rt.getX() - lb.getX()) * (rt.getY() - lb.getY()));
+
+  double c1 = (rt.getX() + lb.getX())/2;
+  double c2 = (rt.getY() + lb.getY())/2;
+}
