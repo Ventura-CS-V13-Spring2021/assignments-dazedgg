8167119b7f00d65801c7a3175bbfbfd502c098dd
Zakhar Gazizov <>
2021-05-11 02:54:14 +0000

    still working on student.cpp following similar format to the other ones i did

--- student.cpp
+++ student.cpp
@@ -2,3 +2,26 @@
 #include "student.hpp"
 
 using namespace std;
+
+Student::Student(){
+
+  sname = " ";
+  id = 0;
+}
+
+Student::Student(string stname, int idnum, DOB date, Address addr) {
+
+sname = stname;
+id = idnum;
+dob = date;
+address = addr;
+
+
+}
+
+int Student::getID()const {
+
+  return id;
+
+}
+
