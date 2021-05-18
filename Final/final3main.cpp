b9480c408f4edec430dee4f5a9f6ca74b632f28c
Zakhar Gazizov <>
2021-05-18 21:05:54 +0000

    more bug fixes

--- Final/final3main.cpp
+++ Final/final3main.cpp
@@ -6,10 +6,14 @@ Student obj1, obj2;
 obj1.Input();
 cout << "Student 1's information: " << endl;
 obj1.printAll();
-
+cout << endl;
 obj2 = obj1;
 
+cout << "Student 2's information from student 1: " << endl;
+obj2.printAll();
 obj1.reset();
+
+cout << endl;
 
 cout << "Student 1's information after clearing: " << endl;
 obj1.printAll();
@@ -17,5 +21,5 @@ obj1.printAll();
 cout << "Student 2's information: " << endl;
 obj2.printAll();
 
-
+return 0;
 }
