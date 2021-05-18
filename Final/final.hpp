e1f091ebbfcf9ea593223299f7286bbb2b582a73
Zakhar Gazizov <>
2021-05-18 20:37:18 +0000

    worked on output and operator

--- Final/final.hpp
+++ Final/final.hpp
@@ -16,6 +16,7 @@ private:
 
 
 public: 
+Student();
 
     ~Student();
     void Input();
--- Final/final3.cpp
+++ Final/final3.cpp
@@ -6,3 +6,44 @@
 
 using namespace std;
 
+Student::Student() : name(" "), num_classes(0), class_list(NULL){}
+
+Student::~Student(){
+
+  num_classes = 0;
+  reset();
+  name = "";
+}
+
+void Student::reset() {
+  if (class_list) {
+    delete[] class_list;
+    class_list = NULL;
+    num_classes = 0;
+
+
+  }
+}
+
+void Student::Output() {
+
+  cout << "Class list: " << endl;
+  for (int i = 0; i < num_classes; i++) {
+    cout << right << i + 1 << class_list[i] << endl;
+  }
+}
+
+Student& Student::operator =(const Student& rhs)
+{
+    reset();
+    num_classes = rhs.num_classes;
+    if (num_classes > 0) {
+        class_list = new string[num_classes];
+        for (int i = 0; i < num_classes; i++) {
+            class_list[i] = rhs.class_list[i];
+        }
+    }
+  
+    return *this;
+
+}
