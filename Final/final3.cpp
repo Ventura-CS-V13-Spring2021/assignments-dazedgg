1c70c0fc5d5277e14547030a5772d052d0d3d8e8
Zakhar Gazizov <>
2021-05-18 20:16:27 +0000

    making the constructors in final3.cpp

--- Final/final3.cpp
+++ Final/final3.cpp
@@ -6,4 +6,28 @@
 
 using namespace std;
 
+Student::~Student() {
+  resetClass();
+}
 
+Student::Student(const Student &rhs) {
+
+  num_classes = rhs.num_classes;
+  name = rhs.name;
+}
+
+Student::~Student() {
+
+  delete[] class_list;
+}
+
+Student &Student::operator=(const Student &rhs) 
+{
+
+}
+
+void Student::printAll()const{
+
+  cout <<"Student Name:"<< name << endl;
+  cout << "Class List: " << endl
+}
